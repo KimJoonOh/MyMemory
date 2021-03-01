542c5ba47fdedaeef5a2f5ba1f728a5b35676edb
Joon <kjo11300@naver.com>
2021-03-01 14:16:37 +0900

    신규 생성

---
+++ MyMemory/MemoCell.swift
@@ -0,0 +1,23 @@
+//
+//  MemoCell.swift
+//  MyMemory
+//
+//  Created by Kim JoonOh on 2021/03/01.
+//
+
+import UIKit
+
+class MemoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
