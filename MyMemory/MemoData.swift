e12a221bfed2824fb21830a4c911afab25496a00
Joon <kjo11300@naver.com>
2021-03-01 14:15:40 +0900

    신규 생성

---
+++ MyMemory/MemoData.swift
@@ -0,0 +1,18 @@
+//
+//  MemoData.swift
+//  MyMemory
+//
+//  Created by Kim JoonOh on 2021/03/01.
+//
+
+import UIKit
+
+class MemoData {
+    var memoIdx : Int? //데이터 식별값
+    var title: String?  //메모 제목
+    var contents : String? //메모내용
+    var image : UIImage? //이미지
+    var regdate : Date? //작성일
+    
+    
+}
