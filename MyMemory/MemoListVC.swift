2cfc69c9cc57907931604e3f80b5741f1e14a678
Joon <kjo11300@naver.com>
2021-03-04 02:00:27 +0900

    왜안되는지 못찾음

--- MyMemory/MemoListVC.swift
+++ MyMemory/MemoListVC.swift
@@ -9,14 +9,15 @@ import UIKit
 
 class MemoListVC: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.tableView.reloadData()
     }
 
     // MARK: - Table view data source
@@ -27,19 +28,27 @@ class MemoListVC: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        let count = self.appDelegate.memolist.count
+        return count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        
+        let row = self.appDelegate.memolist[indexPath.row]
+        let cellID = row.image == nil ? "memoCell" : "memoCellWithImage"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID) as! MemoCell
+        
+        cell.subject?.text = row.title
+        cell.contents?.text = row.contents
+        cell.img?.image = row.image
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        cell.regdate?.text = formatter.string(from: row.regdate!)
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
