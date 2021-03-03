c3617aa5c3fa849c0d773b0ad1d01680b75363ce
Joon <kjo11300@naver.com>
2021-03-04 02:14:18 +0900

    안되는부분 수정

--- MyMemory/MemoListVC.swift
+++ MyMemory/MemoListVC.swift
@@ -10,45 +10,42 @@ import UIKit
 class MemoListVC: UITableViewController {
 
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
-    
+
 //    override func viewDidLoad() {
 //        super.viewDidLoad()
 //
 //    }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         self.tableView.reloadData()
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
+
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let count = self.appDelegate.memolist.count
         return count
     }
 
-    
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+
         let row = self.appDelegate.memolist[indexPath.row]
         let cellID = row.image == nil ? "memoCell" : "memoCellWithImage"
         let cell = tableView.dequeueReusableCell(withIdentifier: cellID) as! MemoCell
-        
+
         cell.subject?.text = row.title
         cell.contents?.text = row.contents
         cell.img?.image = row.image
-        
+
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
         cell.regdate?.text = formatter.string(from: row.regdate!)
         return cell
     }
-    
+
 
     /*
     // Override to support conditional editing of the table view.
@@ -66,7 +63,7 @@ class MemoListVC: UITableViewController {
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
     */
 
